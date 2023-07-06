85b6544116321e4fbe745dc1b80f7175ebe2bfc5
nader-safa <125364885+nader-safa@users.noreply.github.com>
2023-07-07 01:42:30 +0300

    updated user schema

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,53 +1,100 @@
-import User from "../models/user.js";
-import Developer from "../models/developer.js";
-import Attachment from "../models/attachment.js";
+import User from '../models/user.js'
+import Developer from '../models/developer.js'
+import Attachment from '../models/attachment.js'
+
+import { v4 as uuidv4 } from 'uuid'
+
+function generateToken(type) {
+  const token = uuidv4()
+  const expiresAt = new Date(Date.now() + 1000 * 60 * 60)
+  return {
+    token: token,
+    expiresAt: expiresAt,
+    type: type,
+  }
+}
+
+function decryptToken(token) {
+  const parts = token.split('.')
+  if (parts.length !== 3) {
+    // Invalid token
+    return null
+  }
+
+  const tokenId = parts[0]
+  const expiresAt = parts[1]
+  const type = parts[2]
+
+  if (uuid.isValid(tokenId) && new Date() < new Date(expiresAt)) {
+    // Token is valid
+    return type
+  } else {
+    // Token is expired
+    return null
+  }
+}
 
 const getDeveloperTitle = (req, res) => {
   User.findOne({ uid: req.params.user_id }, (error, user) => {
     if (error) {
-      console.log(error);
-      res.status(500).send(error);
+      console.log(error)
+      res.status(500).send(error)
     } else if (user) {
       if (user.developerId) {
         Developer.findById(user.developerId, (error, developer) => {
-          res.send(developer.title);
-        });
+          res.send(developer.title)
+        })
       } else {
-        res.send(404);
+        res.send(404)
       }
     } else {
-      res.status(404).send({ message: `User not found` });
+      res.status(404).send({ message: `User not found` })
     }
-  });
-};
+  })
+}
 
 // Handle index actions
 const findAll = (req, res) => {
   User.find({}, (error, users) => {
     if (error) {
-      console.log(error);
-      res.status(500).send(error);
+      console.log(error)
+      res.status(500).send(error)
     } else {
-      console.log(req.params);
-      req.query.token === "wvGe5xbCkyiDcpnFTbzex8iQsYHceSYR"
+      console.log(req.params)
+      req.query.token === 'wvGe5xbCkyiDcpnFTbzex8iQsYHceSYR'
         ? res.send(users)
-        : res.send("Success");
+        : res.send('Success')
     }
-  });
-};
+  })
+}
 // Handle view user info
+// const findOne = (req, res) => {
+//   // Get the user ID from the request
+//   const userId = req.params.userId
+
+//   // Find the user by the ID
+//   const user = User.findById(userId)
+//   if (!user) {
+//     // User not found
+//     res.status(404).send('User not found')
+//   }
+
+//   // Return the user object
+//   res.json(user)
+// }
+
 const findOne = (req, res) => {
   User.findOne({ uid: req.params.user_id }, (error, user) => {
     if (error) {
-      console.log(error);
-      res.status(500).send(error);
+      console.log(error)
+      res.status(500).send(error)
     } else if (user) {
-      res.send(user);
+      res.send({ user: user, ...generateToken(user.type) })
     } else {
-      res.status(404).send({ message: `User not found` });
+      res.status(404).send({ message: `User not found` })
     }
-  });
-};
+  })
+}
 
 const create = (req, res) => {
   // for (let step = 0; step < 5000; step++) {
@@ -55,87 +102,89 @@ const create = (req, res) => {
     ...req.body,
   }).save((error, user) => {
     if (error || !user) {
-      console.log(error);
-      res.status(500).send(error);
+      console.log(error)
+      res.status(500).send(error)
     } else {
       res.send({
-        message: "User created successfully",
+        message: 'User created successfully',
         user: user,
-      });
-      console.log("success");
+      })
+      console.log('success')
     }
-  });
+  })
   // }
-};
+}
 // Handle update user info
 const update = (req, res) => {
   User.findById(req.params.user_id, (error, user) => {
     if (error) {
-      console.log(error);
-      res.status(500).send(error);
+      console.log(error)
+      res.status(500).send(error)
     } else if (user) {
       Object.keys(req.body).forEach((key) => {
         if (req.body) {
-          user[key] = req.body[key];
+          user[key] = req.body[key]
         }
-      });
-      User.save((error, user) => {
+      })
+
+      console.log(user)
+      user.save((error, user) => {
         if (error || !user) {
-          console.log(error);
-          res.status(500).send(error);
+          console.log(error)
+          res.status(500).send(error)
         } else {
           res.send({
-            message: "User updated successfully",
+            message: 'User updated successfully',
             user: user,
-          });
+          })
         }
-      });
+      })
     } else {
-      res.status(404).send({ message: "User not found" });
+      res.status(404).send({ message: 'User not found' })
     }
-  });
-};
+  })
+}
 // Handle delete user
 const destroy = (req, res) => {
   User.remove({ _id: req.params.user_id }, (error) => {
     if (error) {
-      console.log(error);
-      res.status(500).send(error);
+      console.log(error)
+      res.status(500).send(error)
     } else {
       res.send({
-        message: "User deleted successfully",
-      });
+        message: 'User deleted successfully',
+      })
     }
-  });
-};
+  })
+}
 
 const getUserRegisteredEventIds = (req, res) => {
   User.findOne({ uid: req.params.user_id }, (error, user) => {
     if (error) {
-      console.log(error);
-      res.status(500).send(error);
+      console.log(error)
+      res.status(500).send(error)
     } else if (user) {
-      res.send(user.registeredEventIds);
+      res.send(user.registeredEventIds)
     } else {
-      res.status(404).send({ message: `User not found` });
+      res.status(404).send({ message: `User not found` })
     }
-  });
-};
+  })
+}
 
 const registerNotificationToken = (req, res) => {
   User.findOne({ uid: req.params.user_id }, (error, user) => {
     if (error) {
-      console.log(error);
-      res.status(500).send(error);
+      console.log(error)
+      res.status(500).send(error)
     } else if (user) {
-      user.notificationToken = req.body.notificationToken;
-      user.save();
-      res.send({ message: "Notification registered successfully" });
+      user.notificationToken = req.body.notificationToken
+      user.save()
+      res.send({ message: 'Notification registered successfully' })
     } else {
-      res.status(404).send({ message: `User not found` });
+      res.status(404).send({ message: `User not found` })
     }
-  });
-};
+  })
+}
 
 export default {
   findAll,
@@ -146,4 +195,4 @@ export default {
   getDeveloperTitle,
   getUserRegisteredEventIds,
   registerNotificationToken,
-};
+}
--- src/models/user.js
+++ src/models/user.js
@@ -1,46 +1,22 @@
-import mongoose from "mongoose";
-import { registrationSchema } from "./registration.js";
-import Event from "./event.js";
-import { developerSchema } from "./developer.js";
-const Schema = mongoose.Schema;
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  fullName: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  notificationToken: {
-    type: String,
-  },
-  admin: {
-    type: Boolean,
-    default: false,
-  },
-  adminPassword: {
-    type: String,
-  },
-  uid: {
-    type: String,
-  },
-  role: {
-    type: String,
-  },
-  eventId: {
-    type: Schema.ObjectId,
-    ref: "Event",
-  },
-  registeredEventIds: [{ type: String, default: [] }],
-  developerId: { type: Schema.ObjectId, ref: "Developer" },
-  registrations: [registrationSchema],
-  create_date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  email: { type: String, required: true, unique: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  nick_name: { type: String },
+  birth_date: { type: Date },
+  gender: { type: String, enum: ['M', 'F'] },
+  type: { type: String, enum: ['player', 'coach', 'admin'], default: 'player' },
+  admin: { type: Boolean, default: false },
+  profile_picture_url: { type: String },
+  club_id: { type: mongoose.Types.ObjectId },
+  preferred_hand: { type: String, enum: ['R', 'L'] },
+  height: { type: Number, min: 140, max: 240 },
+  approved: { type: Boolean, default: false },
+})
 
-export { userSchema };
-export default mongoose.model("User", userSchema);
+export { userSchema }
+export default mongoose.model('User', userSchema)
